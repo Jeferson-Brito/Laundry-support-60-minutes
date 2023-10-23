c50c0105d7bd79f184664db049223efb123955dd
Jeferson Brito <jeffersonbrito2455@gmail.com>
2023-10-23 09:16:33 -0300

    Adicionar arquivos do projeto

---
+++ js/cards-maquineta.js
@@ -0,0 +1,7 @@
+function openPopup(popupNumber) {
+    document.getElementById("popup" + popupNumber).style.display = "block";
+  }
+
+  function closePopup(popupNumber) {
+    document.getElementById("popup" + popupNumber).style.display = "none";
+  }
